4728fc1a06c32383695eb6d2e28d530501f57479
TerminalHash <lyashuk.voxx@gmail.com>
2023-03-07 21:58:20 +0300

    Cherrypick changes from forked repo

--- render-pngs_Inkscape-before-v1.py
+++ render-pngs_Inkscape-before-v1.py
@@ -66,14 +66,14 @@ class SVGRect:
 		dbg("New SVGRect: (%s)" % name)
 
 	def renderFromSVG(self, svgFName, sliceFName):
-		rc = os.system('inkscape --export-id="%s" --export-filename="pngs/24/%s" --export-type=png "%s"' % (self.name, sliceFName, svgFName))
+		rc = os.system('inkscape --without-gui --export-id="%s" --export-png="pngs/24/%s" "%s"' % (self.name, sliceFName, svgFName))
 		if rc > 0:
 			fatalError('ABORTING: Inkscape failed to render the slice.')
 
 # Not only render 24x4 PNGs, but also 32x32 and 48x48
 
-		rc = os.system('inkscape --export-type=png -w 32 -h 32 --export-id="%s" --export-filename="pngs/32/%s" "%s"' % (self.name, sliceFName, svgFName))
-		rc = os.system('inkscape --export-type=png -w 48 -h 48 --export-id="%s" --export-filename="pngs/48/%s" "%s"' % (self.name, sliceFName, svgFName))
+		rc = os.system('inkscape -w 32 -h 32 --without-gui --export-id="%s" --export-png="pngs/32/%s" "%s"' % (self.name, sliceFName, svgFName))
+		rc = os.system('inkscape -w 48 -h 48 --without-gui --export-id="%s" --export-png="pngs/48/%s" "%s"' % (self.name, sliceFName, svgFName))
 
 class SVGHandler(handler.ContentHandler):
 	"""Base class for SVG parsers"""
@@ -259,7 +259,7 @@ if __name__ == '__main__':
 		rects = svgLayerHandler.svg_rects
 
 	# loop through each slice rectangle, and render a PNG image for it
-	for rect in rects:
+	for rect in svgLayerHandler.svg_rects:
 		sliceFName = sliceprefix + rect.name + '.png'
 
 		dbg('Saving slice as: "%s"' % sliceFName)
